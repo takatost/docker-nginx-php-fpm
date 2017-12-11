ad8861fe53fa3e53ee97d7d074187475fe2b86b2
John Wang <takatost@users.noreply.github.com>
2017-12-11 12:16:14 +0800

    Remove chown

--- scripts/start.sh
+++ scripts/start.sh
@@ -63,9 +63,6 @@ if [ ! -z "$PUID" ]; then
   deluser nginx
   addgroup -g ${PGID} nginx
   adduser -D -S -h /var/cache/nginx -s /sbin/nologin -G nginx -u ${PUID} nginx
-else
-  # Always chown webroot for better mounting
-  chown -Rf nginx.nginx /data/www
 fi
 
 mkdir -p /etc/default
